a6c54c6fcb6367b576a1649c34a0504a634a0e50
Michael Fisher <mafish88@icloud.com>
2024-12-30 04:17:56 -0500

    Add initial deployment configuration and update token service imports

--- app/src/services/token.service.ts
+++ app/src/services/token.service.ts
@@ -1,5 +1,5 @@
-import { DUST_AMOUNT, ExecuteScriptResult, SignerProvider } from '@alephium/web3'
-import { Withdraw } from 'my-contracts'
+import { DUST_AMOUNT, ExecuteScriptResult, SignerProvider } from '@alephium/web3';
+import { Mint, Withdraw } from 'my-contracts/deployments';
 
 export const withdrawToken = async (signerProvider: SignerProvider, amount: string, tokenId: string): Promise<ExecuteScriptResult> => {
   return await Withdraw.execute(signerProvider, {
